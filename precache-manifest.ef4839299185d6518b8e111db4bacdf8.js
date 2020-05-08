7e8ff03cc5c767527b69969802b5acf10643907e
Lyumih <m.ivanov1225@gmail.com>
2020-05-08 23:01:17 +0300

    Updates

---
+++ precache-manifest.ef4839299185d6518b8e111db4bacdf8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a158bc2365014a515f41036952b4d1d9",
+    "url": "/demo-typescript-react-vladilen/index.html"
+  },
+  {
+    "revision": "bee2798ab009d3f22059",
+    "url": "/demo-typescript-react-vladilen/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "38679cd1f4520ab7a688",
+    "url": "/demo-typescript-react-vladilen/static/js/2.935627f9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/demo-typescript-react-vladilen/static/js/2.935627f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bee2798ab009d3f22059",
+    "url": "/demo-typescript-react-vladilen/static/js/main.0f955f1c.chunk.js"
+  },
+  {
+    "revision": "8eb037dc3ee81531d89d",
+    "url": "/demo-typescript-react-vladilen/static/js/runtime-main.feb28860.js"
+  }
+]);
