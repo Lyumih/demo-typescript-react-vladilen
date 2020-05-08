c9a3d3b90545e2c5507e5e231bb24ee7b82cb3a9
Lyumih <m.ivanov1225@gmail.com>
2020-05-08 22:49:36 +0300

    feat(docs): initial project

---
+++ docs/precache-manifest.c1b4f3be35d17b6da38b554cb5e389d6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e88b88603cc0231faa413bed1dc53c47",
+    "url": "/docs/index.html"
+  },
+  {
+    "revision": "0606d01b4e787bb07f78",
+    "url": "/docs/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "38679cd1f4520ab7a688",
+    "url": "/docs/static/js/2.935627f9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/docs/static/js/2.935627f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0606d01b4e787bb07f78",
+    "url": "/docs/static/js/main.a802792d.chunk.js"
+  },
+  {
+    "revision": "1fd91af73e395c7e8457",
+    "url": "/docs/static/js/runtime-main.eebfef55.js"
+  }
+]);
